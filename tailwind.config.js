66ad5dd741be27ddfaad410a96dd155c1588b461
farhanlumoshive <farhan.arfianto@lumoshive.com>
2026-05-18 22:18:49 +0700

    style: update theme to earth tones palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        sage: '#829460',
+        cream: '#F3EFE0',
+        terracotta: '#BC8F8F',
+      },
       fontFamily: {
         serif: ['"Playfair Display"', 'serif'],
         sans: ['"Inter"', 'sans-serif'],
